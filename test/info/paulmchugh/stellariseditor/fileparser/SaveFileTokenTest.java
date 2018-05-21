c5c27ec824b49bedf0ef45e62e122e42b8fd29f7
paul-mchugh <paulpjmmchugh@gmail.com>
2018-05-21 12:59:06 -0700

    Added Tokenizer Test

---
+++ test/info/paulmchugh/stellariseditor/fileparser/SaveFileTokenTest.java
@@ -0,0 +1,12 @@
+package test.info.paulmchugh.stellariseditor.fileparser;
+
+import org.junit.Test;
+
+public class SaveFileTokenTest
+{
+	@Test
+	public void testTokenQueueGenerate()
+	{
+		System.out.print("Test");
+	}
+}
