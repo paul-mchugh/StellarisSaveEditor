03125270705d221c26ca74f4f5bab46d5c59f6b7
paul-mchugh <paulpjmmchugh@gmail.com>
2018-08-06 15:06:38 -0700

    Added tests for the named group

--- test/info/paulmchugh/stellariseditor/datatypes/NamedGroupSaveTest.java
+++ test/info/paulmchugh/stellariseditor/datatypes/NamedGroupSaveTest.java
@@ -4,6 +4,8 @@ import info.paulmchugh.stellariseditor.datatypes.*;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class NamedGroupSaveTest
 {
 	@Test
@@ -52,5 +54,54 @@ public class NamedGroupSaveTest
 		{
 			Assert.assertEquals(SaveFailureReason.EMPTY_NAMED_GROUP, e.getSaveFailReason());
 		}
+	}
+	
+	@Test
+	public void test4_quotedKeys() throws StateInvalidForSavingException
+	{
+		String expectedString = "{\n\ti_am_a_string=\"a string\"\n\ti_am_a_float=2.9\n\t\"i am a quoted key and int\"=5\n\t\"i am a qk and string\"=\"hi\"\n}";
+		
+		NamedGroup group = new NamedGroup();
+		
+		group.insert("i_am_a_string", new DSString("a string"));
+		group.insert("i_am_a_float", new DSFloat(2.9));
+		group.insert("\"i am a quoted key and int\"", new DSInteger(5));
+		group.insert("\"i am a qk and string\"", new DSString("hi"));
+		
+		String actualString = group.getSaveRepresentation(0);
+		
+		Assert.assertEquals(expectedString, actualString);
+	}
+	
+	@Test
+	public void test5_printOrder() throws StateInvalidForSavingException
+	{
+		NamedGroup group = new NamedGroup();
+		
+		group.insert("c_key", new DSInteger(5));
+		group.insert("b_key", new DSInteger(5));
+		group.insert("a_key", new DSInteger(5));
+		
+		//test that the result is correct
+		String expectedFirst = "{\n\tc_key=5\n\tb_key=5\n\ta_key=5\n}";
+		String actualFirst = group.getSaveRepresentation(0);
+		Assert.assertEquals(expectedFirst,actualFirst);
+		
+		//test that the print order is correct
+		ArrayList<String> expectedFirstOrder = new ArrayList<>();
+		expectedFirstOrder.add("c_key");
+		expectedFirstOrder.add("b_key");
+		expectedFirstOrder.add("a_key");
+		ArrayList<String> actualFirstOrder  = group.getKeyPrintOrder();
+		Assert.assertEquals(expectedFirstOrder, actualFirstOrder);
+		
+		//clear the print order
+		group.setKeyPrintOrder(new ArrayList<>());
+		
+		//test to see if it is printing in default alphebetical order
+		String expectedSecond = "{\n\ta_key=5\n\tb_key=5\n\tc_key=5\n}";
+		String actualSecond = group.getSaveRepresentation(0);
+		Assert.assertEquals(expectedSecond, actualSecond);
+		
 	}
 }
